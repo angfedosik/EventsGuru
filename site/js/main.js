ee3bbac7eebc397fb9b8c3c8208a4f5660fa0701
Angelina Fedosik <angfedosik@gmail.com>
2017-12-27 10:25:17 +0300

    initial commit

---
+++ site/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  
+   $("#anim-text").typed({
+    strings: ["EventsGuru", "Making every event better"],
+    typeSpeed: 7,
+    backDelay: 900,
+    backSpeed: 1,
+    loop: true,
+    startDelay: 1
+  });
+
+  $("#scenary").typed({
+    strings: ["We will make you a scenary", "It will be something like this" ,"...", "...",
+     "First verse of scenary" ,"..." ,"..." ,"Oh no, it's bad",
+     "Second verse of scenary" ,"..." ,"..." ,"Oh, it's better",
+     "Fine" , "We are sending it to you" , "Поставьте зачет", "))))"],
+    typeSpeed: 12,
+    backDelay: 900,
+    backSpeed: 1,
+    loop: true,
+    startDelay: 1
+  });
+  
+  
+  let header = document.getElementsByTagName('header')[0];
+  window.onscroll = () => {
+    let h =  window.pageYOffset;
+    if(h>100)
+      header.classList.add('active');
+    else  
+      header.classList.remove('active');
+  }
+});
---
+++ tourtropa-express/routes/order.js
@@ -0,0 +1,67 @@
+
+var express = require('express');
+var faker = require('faker');
+
+var router = express.Router();
+
+
+
+router.get('/', function(req, res) {
+  res.send([
+  {	
+  "user":"Vlad",
+	"price": "800",
+	"id":"452124h",
+	"services": 
+		[
+			{
+				"name" : "costumes",
+				"size":"XL",
+				"price": "500"
+			},
+			{
+				"name":"text",
+				"price":"300",
+				"topic":"Cartoons",
+				"duration": "2 minutes"
+			}
+		],
+
+	"delivery":
+	{	
+		"city":"Minsk",
+		"delivered": "true",
+		"time":"iso + date forme: 2017-09-08T10:20Z+3" 
+	}
+},
+{	
+	"user":"Elena",
+	"price": "300",
+	"id":"57642124h",
+	"services": 
+			{
+				"name":"text",
+				"price":"300",
+				"topic":"Films",
+				"duration": "3 minutes"
+			},
+
+	"delivery":
+	{	
+		"city":"Brest",
+		"delivered": "true",
+		"time":"iso + date forme: 2017-08-17T15:20Z+3" 
+	}
+}
+]
+  );
+});
+
+router.get('/:id', function(req, res) {
+  res.send({ id: req.params.id,
+              address: faker.internet.email(),
+             format: req.query.details});
+});
+
+
+module.exports = router;
